9d4ca5f20a1ac55e6a384953166ca6db84f7c6fe
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-08-25 22:12:10 +0500

    complete vide and content of hero section now move to animate the content

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,16 +9,16 @@ const Hero = () => {
         controls=""
         className="w-[100%] h-[100%] object-cover"
       ></video>
-      <div className="lg:max-w-[900px] mx-auto flex flex-col items-center justify-center absolute top-[15%] left-[22%]">
+      <div className="lg:max-w-[900px] mx-auto flex flex-col items-center justify-center absolute top-[16%] left-[23.8%]">
         <div>
-          <h1 className="title">Freaking Delicious</h1>
+          <h1 className="title tracking-normal">Freaking Delicious</h1>
         </div>
-        <div className="bg-[var(--color-yellow-brown)] w-[96%] flex justify-center items-center rotate-358 outline-[10px] outline-solid outline-[#faeade]">
+        <div className="bg-[var(--color-yellow-brown)] w-[96%] flex justify-center items-center  mt-[0.3rem] rotate-357 outline-[7px] outline-solid outline-[#faeade]">
           <h1 className="title text-[#fce1cd] pb-[0.8rem]">
             Protein + Caffeine
           </h1>
         </div>
-        <p className="des text-center my-[4rem]">
+        <p className="des text-center m-[3rem_0rem_4rem_0rem]  w-[52%]">
           Live life to the fullest with SPYLT: Shatter boredom and embrace your
           inner kid with every deliciously smooth chug.
         </p>
