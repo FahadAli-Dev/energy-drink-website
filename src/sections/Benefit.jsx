b49f7dbf431aeefb9efa6b2d551b96e78c6e5031
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-09-12 22:02:23 +0500

    working on Benefit-section

--- src/sections/Benefit.jsx
+++ src/sections/Benefit.jsx
@@ -1,5 +1,17 @@
 const Benefit = () => {
-  return <section className="w-[100%] h-[120vh] bg-[#222123]"></section>;
+  return (
+    <section className="b-wrapper w-[100%] h-[120vh] bg-[#222123]">
+      <div className="b-container">
+        <span>
+          Unlock the Advantages: Explore the Key Benefits of Choosing SPYLT
+        </span>
+        <span className="title">Shelf stable</span>
+        <span className="title">Protein + Caffeine</span>
+        <span className="title">Infinitely recyclable</span>
+        <span className="title">Lactose free</span>
+      </div>
+    </section>
+  );
 };
 
 export default Benefit;
