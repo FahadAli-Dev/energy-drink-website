4cf085998125808d01f6cc90a39baf6758fb152e
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-09-14 23:00:24 +0500

    working on footer-section

--- src/components/FooterBody.jsx
+++ src/components/FooterBody.jsx
@@ -2,7 +2,14 @@ import Image from "next/image";
 
 const FooterBody = () => {
   return (
-    <div className="w-[100%] h-[111vh]">
+    <div className="w-[100%] h-[132vh] relative overflow-hidden bg-[var(--color-black)]">
+      <video
+        src="/videos/splash.mp4"
+        autoPlay
+        controls=""
+        muted
+        className="absolute top-[-7%] mix-blend-lighten object-contain"
+      />
       <div className="text-center mt-[9.6rem]">
         <span className="title text-[var(--color-milk)] !text-[119px]">
           #chugresponsibly
@@ -34,8 +41,8 @@ const FooterBody = () => {
           />
         </div>
       </div>
-      <div className="grid grid-cols-2 grid-rows-2 gap-y-[10rem] mt-[8rem] p-[2rem]">
-        <div className="flex gap-[3rem]">
+      <div className="grid grid-cols-2 grid-rows-2 gap-y-[3rem] mt-[8rem] p-[2rem] des text-[var(--color-milk)]">
+        <div className="flex gap-[3.5rem]">
           <span>SPYLT Flavors</span>
           <span className="flex flex-col gap-[0.5rem]">
             <span>Chug Club </span>
@@ -48,19 +55,34 @@ const FooterBody = () => {
             <span>Tasty Talk</span>
           </span>
         </div>
-        <div className="flex flex-col ">
-          <label htmlFor="email">
-            Get Exclusive Early Access and Stay Informed About Product Updates,
-            Events, and More!
-          </label>
-          <input type="email" />
+        <div className="flex flex-col items-end">
+          <div className="flex flex-col gap-[3rem] relative">
+            <label htmlFor="email" className="pr-[4rem]">
+              Get Exclusive Early Access and Stay Informed About Product <br />{" "}
+              Updates, Events, and More!
+            </label>
+            <input
+              style={{ wordSpacing: "-3px" }}
+              type="email"
+              placeholder="Enter your email"
+              className="placeholder:text-[#999999] focus:outline-none pb-[15px] border-b-[1px] border-solid borderb-[#d9d9d9] text-[2rem] font-bold font-sans"
+            />
+
+            <Image
+              src="/images/arrow.svg"
+              alt="arrow"
+              width={40.71}
+              height={40.71}
+              className="absolute right-[-1.1%] bottom-[11%]"
+            />
+          </div>
         </div>
-        <div className="flex justify-start items-end">
+        <div className="flex justify-start items-end opacity-[0.5]">
           Copyright © 2025 Spylt - All Rights Reserved
         </div>
-        <div className="flex gap-[3rem] justify-end">
-          <span className="flex items-end">Privacy Policy</span>
-          <span className="flex items-end">Terms of Sеrvice</span>
+        <div className="flex gap-[2rem] justify-end">
+          <span className="flex items-end opacity-[0.5]">Privacy Policy</span>
+          <span className="flex items-end opacity-[0.5]">Terms of Sеrvice</span>
         </div>
       </div>
     </div>
