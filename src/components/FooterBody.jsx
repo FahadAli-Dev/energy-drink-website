dc82c2426c426a5344fcd2d3399406c657915d0a
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-09-14 20:31:08 +0500

    working on footer-section

--- src/components/FooterBody.jsx
+++ src/components/FooterBody.jsx
@@ -1,5 +1,70 @@
+import Image from "next/image";
+
 const FooterBody = () => {
-  return <div></div>;
+  return (
+    <div className="w-[100%] h-[111vh]">
+      <div className="text-center mt-[9.6rem]">
+        <span className="title text-[var(--color-milk)] !text-[119px]">
+          #chugresponsibly
+        </span>
+      </div>
+      <div className="flex gap-[1rem] justify-center mt-[4.3rem]">
+        <div className="w-[76.8px] h-[76.8px] rounded-[999px] hover:bg-[#ffffff1a] cursor-pointer duration-[0.3s] ease-out flex justify-center items-center border-[1px] border-solid border-[#faeade33]">
+          <Image
+            src="/images/yt.svg"
+            alt="youtube"
+            width={30.71}
+            height={30.71}
+          />
+        </div>
+        <div className="w-[76.8px] h-[76.8px] rounded-[999px] hover:bg-[#ffffff1a] cursor-pointer duration-[0.3s] ease-out flex justify-center items-center border-[1px] border-solid border-[#faeade33]">
+          <Image
+            src="/images/insta.svg"
+            alt="instagram"
+            width={30.71}
+            height={30.71}
+          />
+        </div>
+        <div className="w-[76.8px] h-[76.8px] rounded-[999px] hover:bg-[#ffffff1a] cursor-pointer duration-[0.3s] ease-out flex justify-center items-center border-[1px] border-solid border-[#faeade33]">
+          <Image
+            src="/images/tiktok.svg"
+            alt="ticktock"
+            width={30.71}
+            height={30.71}
+          />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 grid-rows-2 gap-y-[10rem] mt-[8rem] p-[2rem]">
+        <div className="flex gap-[3rem]">
+          <span>SPYLT Flavors</span>
+          <span className="flex flex-col gap-[0.5rem]">
+            <span>Chug Club </span>
+            <span>Student Marketing</span>
+            <span>Dairy Dealers</span>
+          </span>
+          <span className="flex flex-col gap-[0.5rem]">
+            <span>Company </span>
+            <span>Contacts</span>
+            <span>Tasty Talk</span>
+          </span>
+        </div>
+        <div className="flex flex-col ">
+          <label htmlFor="email">
+            Get Exclusive Early Access and Stay Informed About Product Updates,
+            Events, and More!
+          </label>
+          <input type="email" />
+        </div>
+        <div className="flex justify-start items-end">
+          Copyright © 2025 Spylt - All Rights Reserved
+        </div>
+        <div className="flex gap-[3rem] justify-end">
+          <span className="flex items-end">Privacy Policy</span>
+          <span className="flex items-end">Terms of Sеrvice</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterBody;
--- src/components/FooterHead.jsx
+++ src/components/FooterHead.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const FooterHead = () => {
   return (
-    <div className="w-[100%] h-[135vh]">
+    <div className="w-[100%] h-[135vh] relative">
       <div className="relative w-[100%] h-[30%]">
         <Image
           src="/images/footer-dip.png"
@@ -19,25 +19,24 @@ const FooterHead = () => {
           className="object-fill"
         />
       </div>
-      <div className="fh-content absolute left-[20%] bottom-[5%] flex flex-col">
+      <div className="fh-content absolute left-[6%] bottom-[9%] flex flex-col">
         <span
           style={{ wordSpacing: "0.8rem" }}
-          className="title p-[0.4rem] text-center !text-[var(--color-milk)] overflow-hidden !leading-[7.8rem] !text-[119px] mt-[-1.7rem]"
+          className="title text-center !text-[var(--color-milk)] overflow-hidden !leading-[7.8rem] !text-[119px] mt-[-1.7rem]"
         >
           Right around
         </span>
 
-        <span
-          style={{ clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)" }}
-          className="title flex justify-center nt-2 mt-[-1.3rem] !leading-[7rem] !text-[119px] bg-[#e9aa56] text-[var(--color-dark-brown)] p-[1rem_0rem_2rem_0rem] rotate-[4deg]"
-        >
+        <span className="title flex justify-center !leading-[8rem] !text-[119px] bg-[#e9aa56] text-[var(--color-dark-brown)] w-[87%] pb-[1rem] rotate-[2.5deg]">
           the corner
         </span>
-        <span className="des text-[var(--color-milk)] my-[3rem]">
+        <span className="des !leading-[1rem] text-[var(--color-milk)] my-[3.5rem]">
           Buy our drinks at your local store or <br /> get them delivered (to
           your door).
         </span>
-        <button className="btn">Find in stores</button>
+        <button className="btn text-[var(--color-milk)] !bg-[var(--color-black)]">
+          Find in stores
+        </button>
       </div>
     </div>
   );
