1be39520fac580cbe1c99df7ea320184ea442404
FahadAli-Dev <214241178+FahadAli-Dev@users.noreply.github.com>
2025-09-19 22:11:19 +0500

    complete desktop-version of testimonial-section

--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -5,114 +5,92 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
+
 const Testimonial = () => {
   useGSAP(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".testimonial-section",
-        start: "top -45%",
+        start: "top 0%",
         end: "top -200%",
         scrub: true,
         pin: true,
       },
     });
-    tl.from(".tvCon-0", {
-      translateY: "120%",
-      left: "50%",
-      translateX: "-50%",
-    })
-      .from(
-        ".tvCon-1",
-        {
-          translateY: "120%",
-          left: "50%",
-          translateX: "-50%",
-        },
-        ">"
-      )
-      .from(
-        ".tvCon-2",
-        {
-          translateY: "120%",
-          left: "50%",
-          translateX: "-50%",
-        },
-        ">"
-      )
-      .from(
-        ".tvCon-3",
-        {
-          translateY: "120%",
-          left: "50%",
-          translateX: "-50%",
-        },
-        ">"
-      )
-      .from(
-        ".tvCon-4",
-        {
-          translateY: "120%",
-          left: "50%",
-          translateX: "-50%",
-        },
-        ">"
-      )
-      .from(
-        ".tvCon-5",
-        {
-          translateY: "120%",
-          left: "50%",
-          translateX: "-50%",
-        },
-        ">"
-      )
-      .from(
-        ".tvCon-6",
-        {
-          translateY: "120%",
-          left: "50%",
-          translateX: "-50%",
-        },
-        ">"
-      );
 
     gsap.to(".th-1", {
-      x: 550,
+      translateX: 240,
+      ease: "linear",
       scrollTrigger: {
         trigger: ".testimonial-section",
         start: "top 80%",
-        end: "top -80%",
+        end: "top 0%",
         scrub: true,
       },
     });
 
     gsap.to(".th-2", {
-      x: 300,
+      translateX: 100,
+      ease: "linear",
       scrollTrigger: {
         trigger: ".testimonial-section",
         start: "top 35%",
-        end: "top -130%",
+        end: "top 0%",
         scrub: true,
       },
     });
 
-    gsap.to(".th-3", {
-      x: -650,
-      scrollTrigger: {
-        trigger: ".testimonial-section",
-        start: "top -5%",
-        end: "top -110%",
-        scrub: true,
-      },
-    });
+    tl.to(".th-1", {
+      marginLeft: "30rem",
+      translateY: -600,
+      ease: "linear",
+      duration: 3.5,
+    })
+      .to(
+        ".th-2",
+        {
+          marginLeft: "10.5rem",
+          translateY: -600,
+          ease: "linear",
+          duration: 3.5,
+        },
+        "<"
+      )
+
+      .from(
+        ".th-3",
+        {
+          translateY: "100%",
+          left: "50%",
+          translateX: "-50%",
+          duration: 3.5,
+          ease: "linear",
+        },
+        "<"
+      )
+
+      .from(
+        ".tvCon-0 ,.tvCon-1 ,.tvCon-2 ,.tvCon-3 ,.tvCon-4 ,.tvCon-5 ,.tvCon-6",
+        {
+          translateY: "120%",
+          left: "50%",
+          translateX: "-50%",
+          stagger: 0.5,
+        },
+        "<"
+      );
   });
   return (
-    <section className="mt-[-100vh] testimonial-section relative z-[2] pt-[5rem] pb-[1.9rem] w-[100%] h-[150vh] bg-[var(--color-milk)]">
-      <div className="font-bold text-[20rem] uppercase leading-[20rem] flex flex-col items-center">
-        <h1 className="th-1 text-[var(--color-black)]">What's</h1>
-        <h1 className="th-2 text-[var(--color-light-brown)]">Everyone</h1>
-        <h1 className="th-3 text-[var(--color-black)]">Talking</h1>
-      </div>
+    <section className="mt-[-100vh] testimonial-section relative z-[2] w-[100%] h-[100vh] bg-[var(--color-milk)]">
+      <h1 className="th-1 left-1/2 translate-x-[-50%] top-[10%] text-[var(--color-black)] absolute font-bold text-[20rem] uppercase leading-[20rem]">
+        What's
+      </h1>
+      <h1 className="th-2 left-1/2 translate-x-[-50%] bottom-[1%] text-[var(--color-light-brown)] absolute font-bold text-[20rem] uppercase leading-[20rem]">
+        Everyone
+      </h1>
+      <h1 className="th-3 bottom-[1%] left-[-22%] text-[var(--color-black)] absolute font-bold text-[20rem] uppercase leading-[20rem]">
+        Talking
+      </h1>
       <div className="flex w-[90%] h-[640px] left-1/2 translate-x-[-50%] absolute bottom-[10%]">
         {testimonialData.map((e, i) => {
           return (
@@ -130,6 +108,8 @@ const Testimonial = () => {
                 muted
                 controls=""
                 className="object-cover h-[100%]"
+                onMouseEnter={(e) => e.currentTarget.play()}
+                onMouseLeave={(e) => e.currentTarget.pause()}
               ></video>
               <div className="flex gap-[0.3rem] absolute left-[5%] items-center bottom-[5%]">
                 <Image
@@ -146,7 +126,7 @@ const Testimonial = () => {
           );
         })}
       </div>
-      <button className="btn absolute bottom-[6%] left-1/2 translate-x-[-50%]">
+      <button className="btn absolute bottom-[4%] left-1/2 translate-x-[-50%]">
         Explore all
       </button>
     </section>
